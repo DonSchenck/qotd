095ccab819698f544246ec7d74622db5d922daec
Don Schenck <don.schenck@gmail.com>
2023-04-05 15:43:41 +0000

    Fixed Cowbell quote author

--- main.go
+++ main.go
@@ -6,13 +6,13 @@ import (
 )
 
 var quotes = Quotes{
-	Quote{Id: 0, Quotation: "I got a fever, and the only prescription is MOAR COWBELL!", Author: "Will Ferrell"},
+	Quote{Id: 0, Quotation: "I got a fever, and the only prescription is MOAR COWBELL!", Author: "Christopher Walken"},
 	Quote{Id: 1, Quotation: "Knowledge is power.", Author: "Francis Bacon"},
 	Quote{Id: 2, Quotation: "Life is really simple, but we insist on making it complicated.", Author: "Confucius"},
 	Quote{Id: 3, Quotation: "This above all, to thine own self be true.", Author: "William Shakespeare"},
-	Quote{Id: 4, Quotation: "Never complain. Never explain.", Author: "Katharine Hepburn"},
+	Quote{Id: 4, Quotation: "Never complain, never explain.", Author: "Katharine Hepburn"},
 	Quote{Id: 5, Quotation: "Fly me to the moon...", Author: "Frank Sinatra"},
-	Quote{Id: 6, Quotation: "Seventy-eight percent of internet quotes are made up.", Author: "Abraham Lincoln"},
+	Quote{Id: 6, Quotation: "THese are the teims that try mens souls...", Author: "Thomas Paine"},
 }
 
 func main() {
